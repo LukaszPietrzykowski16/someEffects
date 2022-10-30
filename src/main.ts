7dc54d72c785ef97479f7f0d27a6749df6f78992
Lukasz Pietrzykowski <lukaszpietrzykowsk@gmail.com>
2022-10-30 20:22:54 +0100

    my honest reaction to this commit :

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,21 @@
 import './style.css'
-import img0 from '../images/frame_00_delay-0.1s.gif'
-import img1 from '../images/frame_01_delay-0.1s.gif'
-import img2 from '../images/frame_02_delay-0.1s.gif'
-import img3 from '../images/frame_03_delay-0.1s.gif'
-import img4 from '../images/frame_04_delay-0.1s.gif'
-import img5 from '../images/frame_05_delay-0.1s.gif'
-import img6 from '../images/frame_06_delay-0.1s.gif'
-import img7 from '../images/frame_07_delay-0.1s.gif'
-import img8 from '../images/frame_08_delay-0.1s.gif'
-import img9 from '../images/frame_09_delay-0.1s.gif'
-import img10 from '../images/frame_10_delay-0.1s.gif'
-import img11 from '../images/frame_11_delay-0.1s.gif'
-import img12 from '../images/frame_12_delay-0.1s.gif'
-import img13 from '../images/frame_13_delay-0.1s.gif'
-import img14 from '../images/frame_14_delay-0.1s.gif'
-import img15 from '../images/frame_15_delay-0.1s.gif'
-import img16 from '../images/frame_16_delay-0.1s.gif'
-import img17 from '../images/frame_17_delay-0.1s.gif'
-import img18 from '../images/frame_18_delay-0.1s.gif'
-import img19 from '../images/frame_19_delay-0.1s.gif'
-import img20 from '../images/frame_20_delay-0.1s.gif'
-import img21 from '../images/frame_21_delay-0.1s.gif'
-import img22 from '../images/frame_22_delay-0.1s.gif'
-import img23 from '../images/frame_23_delay-0.1s.gif'
-import img24 from '../images/frame_24_delay-0.1s.gif'
-import img25 from '../images/frame_25_delay-0.1s.gif'
-import img26 from '../images/frame_26_delay-0.1s.gif'
-import img27 from '../images/frame_27_delay-0.1s.gif'
-import img28 from '../images/frame_28_delay-0.1s.gif'
-import img29 from '../images/frame_29_delay-0.1s.gif'
-import img30 from '../images/frame_30_delay-0.1s.gif'
 
 
 
-const images = [img0, img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16, img17, img18, img19, img20, img21, img22, img23, img24, img25, img26, img27, img28, img29, img30]
+const imageContainer = document.querySelector<HTMLElement>('.image-container')
 
+
+function changeImage(){
+  const y = window.scrollY;
+  const label = Math.min(Math.floor(y/59) + 1, 59)
+
+  console.log(label)
+  if (imageContainer !== null){
+    imageContainer.style.backgroundImage = `url('images/frame_${label}_delay-0.1s.gif')`
+  }
+  
+
+  window.addEventListener('scroll', changeImage)
+}
+
+changeImage()
