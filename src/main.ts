da249bf01bd5c43970f880b7a3f61a704dbace08
Lukasz Pietrzykowski <lukaszpietrzykowsk@gmail.com>
2022-10-30 23:09:25 +0100

    new effects

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,20 @@ window.addEventListener('scroll', () => {
   const imageContainer = document.querySelector<HTMLElement>('.image-container')
   const y = window.scrollY;
   const label = Math.min(Math.floor(y/59) + 1, 59)
-  const value = 131;
+  
+  console.log(Math.cos(label / 100))
+
 
   if (imageContainer !== null){
     imageContainer.style.backgroundImage = `url('images/frame_${label}_delay-0.1s.gif')`
+    if (label < 30) {
+      imageContainer.style.transform = `scale(1.0)`
+    }
     if (label > 30){
-      imageContainer.style.width = `${value - label}%`
+      imageContainer.style.transform = `scale(${Math.cos(label / 60)})`
     }
     if (label > 40) {
-      imageContainer.style.width = `${value - label * 1.4}%`
+      // imageContainer.style.width = `${value - label * 1.4}%`
     }
  
   }
