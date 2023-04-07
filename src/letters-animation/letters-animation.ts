560944a30e7b0162dd24b46633c04d8d7d6d7c34
Lukasz Pietrzykowski <lukaszpietrzykowsk@gmail.com>
2023-04-07 15:44:59 +0200

    feat: hoover letter effect

--- src/letters-animation/letters-animation.ts
+++ src/letters-animation/letters-animation.ts
@@ -1,36 +1,17 @@
 import "./style.css";
 
-const text = "very long text";
-const textContainer = document.querySelector(".text-container");
+const spans = document.querySelectorAll(".span-container span");
 
-window.addEventListener("DOMContentLoaded", () => {
-  for (let i = 0; i < text.length; i++) {
-    const div = document.createElement("div");
-    const span = document.createElement("span");
-    div.appendChild(span);
-    span.innerHTML = text[i];
-    textContainer?.appendChild(div);
-    div.classList.add("text");
-  }
-});
+spans.forEach((span, index) => {
+  span.addEventListener("mouseover", () => {
+    if (index > 0) {
+      spans[index - 1].classList.add("before-span");
+    }
+  });
 
-window.addEventListener("mousemove", (event) => {
-  const mouseX = event.clientX;
-  const mouseY = event.clientY;
-  const text = document.querySelectorAll(".text");
-  text.forEach((letter) => {
-    const rect = letter.getBoundingClientRect();
-    if (
-      mouseX >= rect.left &&
-      mouseX <= rect.right &&
-      mouseY >= rect.top &&
-      mouseY <= rect.bottom
-    ) {
-      animateLetter(letter);
+  span.addEventListener("mouseout", () => {
+    if (index > 0) {
+      spans[index - 1].classList.remove("before-span");
     }
   });
 });
-
-function animateLetter(letter: any) {
-  letter.classList.add("hover-text");
-}
