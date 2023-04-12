03c1f2cd37f31e1f0f8bd462520c2d309348d4fe
Lukasz Pietrzykowski <lukaszpietrzykowsk@gmail.com>
2023-04-12 22:18:40 +0200

    creating 3d effect on hoover

--- src/component-rotate/rotate.ts
+++ src/component-rotate/rotate.ts
@@ -1,10 +1,10 @@
 const component = document.querySelector(".component");
 const textWrapper = document.querySelector<HTMLElement>(".text-wrapper");
 
-component?.addEventListener("mousemove", (event: Event) => {
+textWrapper!.addEventListener("mousemove", (event: Event) => {
   const mouseEvent = event as MouseEvent;
 
-  textWrapper!.style.transform = `rotateY(${mouseEvent.clientX * 0.2}deg)`;
+  textWrapper!.style.transform = `rotateY(${mouseEvent.clientX * 0.009}deg)`;
 
   console.log(mouseEvent.clientX);
 });
