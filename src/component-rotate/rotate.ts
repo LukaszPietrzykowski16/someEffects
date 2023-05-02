9ccef2c0f795fc095e1f7fd01dfe3dc7c83147b6
Lukasz Pietrzykowski <lukaszpietrzykowsk@gmail.com>
2023-05-02 14:44:07 +0200

    cleaning up rotate style

--- src/component-rotate/rotate.ts
+++ src/component-rotate/rotate.ts
@@ -4,14 +4,19 @@ const textWrapper = document.querySelector<HTMLElement>(".text-wrapper");
 textWrapper!.addEventListener("mousemove", (event: Event) => {
   const mouseEvent = event as MouseEvent;
   const divWidth = textWrapper!.offsetWidth;
+  const divLeft = textWrapper!.offsetLeft;
 
-  if (mouseEvent.clientX < divWidth / 2) {
+  if (mouseEvent.clientX < divLeft + divWidth / 2) {
     console.log("left");
-    textWrapper!.style.transform = `rotateY(${mouseEvent.clientX * 0.079}deg)`;
+    textWrapper!.style.transform = `rotateY(${mouseEvent.clientX * 0.039}deg)`;
   } else {
     console.log("right");
     textWrapper!.style.transform = `rotateY(-${mouseEvent.clientX * 0.039}deg)`;
   }
 
   console.log(mouseEvent.clientX, divWidth);
+});
+
+textWrapper!.addEventListener("mouseleave", () => {
+  textWrapper!.style.transform = `rotateY(0deg)`;
 });
