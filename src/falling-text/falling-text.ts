3f191e667aa701bac942441512e5c684d7ea8d66
Lukasz Pietrzykowski <lukaszpietrzykowsk@gmail.com>
2023-05-30 20:05:34 +0200

    suiting everything in class to make it to able to make many clicks at the same time

--- src/falling-text/falling-text.ts
+++ src/falling-text/falling-text.ts
@@ -1,59 +1,76 @@
 import "./style.css";
 
-const body = document.querySelector("body");
+class AnimatedDiv {
+  body;
+  numClicks;
+  constructor() {
+    this.body = document.querySelector("body");
+    this.numClicks = 0;
+    this.registerClickEvent();
+  }
 
-body?.addEventListener("click", (e) => {
-  const mouseX = e.clientX;
-  const mouseY = e.clientY;
+  registerClickEvent() {
+    this.body!.addEventListener("click", (e) => {
+      this.numClicks++;
+      const mouseX = e.clientX;
+      const mouseY = e.clientY;
 
-  const newDiv = document.createElement("div");
-  newDiv.style.left = mouseX + "px";
-  newDiv.style.top = mouseY + "px";
-  newDiv.classList.add("red-block");
+      const newDiv = document.createElement("div");
+      newDiv.style.left = mouseX + "px";
+      newDiv.style.top = mouseY + "px";
+      newDiv.classList.add("red-block");
 
-  const keyframeInXAxis = [
-    { left: `${mouseX}px`, opacity: 1 },
-    { opacit: 0 },
-    { left: `${mouseX + 200}px` },
-  ];
-  const keyframeInYAxis = [
-    { top: `${mouseY}px`, opacity: 1 },
-    { opacit: 0 },
-    { top: `${mouseY - 0.5}px` },
-  ];
+      const keyframeInXAxis = [
+        { left: `${mouseX}px`, opacity: 0 },
+        { opacity: 1 },
+        { left: `${mouseX + 200}px` },
+      ];
+      const keyframeInYAxis = [
+        { top: `${mouseY}px`, opacity: 0 },
+        { opacity: 1 },
+        { top: `${mouseY - 0.5}px` },
+      ];
 
-  const keyframesOpacity = [{ opacity: 1 }, { opacity: 0 }];
+      const keyframesOpacity = [{ opacity: 1 }, { opacity: 0 }];
 
-  const keyFrameEffectX = new KeyframeEffect(
-    document.querySelector(".red-block"),
-    keyframeInXAxis,
-    { duration: 3000, fill: "forwards" }
-  );
-  const keyFrameEffectY = new KeyframeEffect(
-    document.querySelector(".red-block"),
-    keyframeInYAxis,
-    {
-      duration: 3000,
-      easing: "cubic-bezier(1, 1500, 0.8, -1500)",
-      fill: "forwards",
-    }
-  );
+      const keyFrameEffectX = new KeyframeEffect(newDiv, keyframeInXAxis, {
+        duration: 3000,
+        fill: "forwards",
+      });
 
-  const keyframeEffectOpacity = new KeyframeEffect(
-    document.querySelector(".red-block"),
-    keyframesOpacity,
-    { duration: 2500, fill: "forwards" }
-  );
+      const keyFrameEffectY = new KeyframeEffect(newDiv, keyframeInYAxis, {
+        duration: 3000,
+        easing: "cubic-bezier(1, 1500, 0.8, -1500)",
+        fill: "forwards",
+      });
 
-  const animationX = new Animation(keyFrameEffectX, document.timeline);
-  const animationY = new Animation(keyFrameEffectY, document.timeline);
-  const animationOpacity = new Animation(
-    keyframeEffectOpacity,
-    document.timeline
-  );
+      const keyframeEffectOpacity = new KeyframeEffect(
+        newDiv,
+        keyframesOpacity,
+        { duration: 2500, fill: "forwards" }
+      );
 
-  body.appendChild(newDiv);
-  animationX.play();
-  animationY.play();
-  animationOpacity.play();
-});
+      const animationX = new Animation(keyFrameEffectX, document.timeline);
+      const animationY = new Animation(keyFrameEffectY, document.timeline);
+      const animationOpacity = new Animation(
+        keyframeEffectOpacity,
+        document.timeline
+      );
+
+      animationX.play();
+      animationY.play();
+      animationOpacity.play();
+
+      animationOpacity.onfinish = () => {
+        newDiv.remove();
+        this.numClicks--;
+        if (this.numClicks === 0) {
+          this.body!.removeEventListener("click", this.registerClickEvent);
+        }
+      };
+
+      this.body!.appendChild(newDiv);
+    });
+  }
+}
+const animatedDiv = new AnimatedDiv();
