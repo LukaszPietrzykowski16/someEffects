e68c4c2df1f32b1d7eede5615f8164b070815a32
Lukasz Pietrzykowski <lukaszpietrzykowsk@gmail.com>
2023-06-15 19:36:16 +0200

    creating section observer

--- src/text-appear/text-appear.ts
+++ src/text-appear/text-appear.ts
@@ -1,2 +1,15 @@
 import "../style.css";
 import "./style.css";
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      console.log("Section is in view");
+    } else {
+      console.log("Seciton is not in view");
+    }
+  });
+});
+
+const section = document.querySelector(".text-element3");
+observer.observe(section!);
